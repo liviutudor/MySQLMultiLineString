a7c24707e607832069e93e8c6cd8365573e017b7
liviutudor <me@liviutudor.com>
2013-11-02 14:32:39 -0700

    separated form in its own file

--- mysql_multiline.php
+++ mysql_multiline.php
@@ -6,51 +6,9 @@
 <body>
 
 <?php
-	//settings
-	$form_action = "mysql_multiline.php";
-	$rows = 10;
-	$columns = 80;
-	$eliminate_empty = true;
-
-	//convert function
-	function safeMySQLMultiline( $txt ) {
-		$txt = str_replace( "'", "''", $txt );
-		$txt = preg_replace( "/\r/i", "", $txt );
-		$txt = preg_replace( "/\n/i", "' \\\\\\\\n\r'", $txt);
-		//$txt = preg_replace( "/\t/i", "' \\\\\\\\t'", $txt);
-		$txt = "'" . $txt . "'";
-
-		//get rid of empty lines if needed
-		if( $eliminate_empty ) {
-
-		}
-		return stripslashes($txt);
-	}
+ $form_action = "mysql_multiline.php";
+ include 'mysql_multiline_form.php' 
 ?>
-
-<form action="<?php echo $form_action; ?>" method="POST">
-	<label for="mysql_value">Multi-line value</label>
-	<br/>
-<textarea id="mysql_value" name="mysql_value" rows="<?php echo $rows; ?>" cols="<?php echo $columns; ?>">
-<?php
-	if( $_POST["mysql_value"] != "" ) {
-		echo stripslashes($_POST["mysql_value"]);
-	}
-?>
-</textarea>
-	<br/><br/>
-		<label for="converted_value">Converted value</label><br/>
-<textarea id="converted_value" name="converted_value" rows="<?php echo $rows; ?>" cols="<?php echo $columns; ?>">
-<?php
-	if( $_POST["mysql_value"] != "" ) {
-		echo safeMySQLMultiline($_POST["mysql_value"]);
-	}
-?>
-</textarea>
-	<br/>
-
-	<input type="submit" />
-</form>
 
 </body>
 </html>
---
+++ mysql_multiline_form.php
@@ -0,0 +1,49 @@
+<?php
+	//settings
+	global $form_action;
+	if($form_action == '' ) {
+		$form_action = "mysql_multiline_form.php";
+	}
+	$rows = 10;
+	$columns = 80;
+	$eliminate_empty = true;
+
+	//convert function
+	function safeMySQLMultiline( $txt ) {
+		$txt = str_replace( "'", "''", $txt );
+		$txt = preg_replace( "/\r/i", "", $txt );
+		$txt = preg_replace( "/\n/i", "' \\\\\\\\n\r'", $txt);
+		//$txt = preg_replace( "/\t/i", "' \\\\\\\\t'", $txt);
+		$txt = "'" . $txt . "'";
+
+		//get rid of empty lines if needed
+		if( $eliminate_empty ) {
+
+		}
+		return stripslashes($txt);
+	}
+?>
+
+<form action="<?php echo $form_action; ?>" method="POST">
+	<label for="mysql_value">Multi-line value</label>
+	<br/>
+<textarea id="mysql_value" name="mysql_value" rows="<?php echo $rows; ?>" cols="<?php echo $columns; ?>">
+<?php
+	if( $_POST["mysql_value"] != "" ) {
+		echo stripslashes($_POST["mysql_value"]);
+	}
+?>
+</textarea>
+	<br/><br/>
+		<label for="converted_value">Converted value</label><br/>
+<textarea id="converted_value" name="converted_value" rows="<?php echo $rows; ?>" cols="<?php echo $columns; ?>">
+<?php
+	if( $_POST["mysql_value"] != "" ) {
+		echo safeMySQLMultiline($_POST["mysql_value"]);
+	}
+?>
+</textarea>
+	<br/>
+
+	<input type="submit" />
+</form>
